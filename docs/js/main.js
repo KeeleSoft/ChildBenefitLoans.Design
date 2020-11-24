472c1365b301ba26ad5338c4423dee3fe9aa76f6
Team Codea <viraj.jay@gmail.com>
2020-11-24 00:09:14 +0000

    moved the scroll detection function inside cblApp object

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,8 +1,20 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 10) {
-        $('.header').addClass('header--fixed');
+
+
+var cblApp = {    
+    init: function(){
+        this.scrollDetection();
+    },
+    scrollDetection:function(){
+        //scroll detection and header status change
+        $(window).scroll(function(){
+            if ($(window).scrollTop() >= 10) {
+                $('.header').addClass('header--fixed');
+            }
+            else {
+                $('.header').removeClass('header--fixed');
+            }
+        });
     }
-    else {
-        $('.header').removeClass('header--fixed');
-    }
-});
+};
+
+cblApp.init();
