4b78a5b140811f550f71fb74c1f9c2450a054ce2
Team Codea <viraj.jay@gmail.com>
2020-12-01 00:55:08 +0000

    eligibility page content added

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,6 +3,7 @@
 var cblApp = {    
     init: function(){
         this.scrollDetection();
+        this.loanEligibilityCheck();
     },
     scrollDetection:function(){
         //scroll detection and header status change
@@ -12,6 +13,16 @@ var cblApp = {
             }
             else {
                 $('.header').removeClass('header--fixed');
+            }
+        });
+    },
+    loanEligibilityCheck:function(){
+        $('.js-eligibility-check').change(function(){
+            if ($('.js-eligibility-check:checked').length === $('.js-eligibility-check').length) {
+                $('.js-postcode-display').removeClass('js-hidden');
+            }
+            else{
+                $('.js-postcode-display').addClass('js-hidden');
             }
         });
     }
